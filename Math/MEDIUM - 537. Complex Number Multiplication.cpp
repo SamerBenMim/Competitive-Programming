f1d2469aa524258160dc6c2f18a8b2bfee1431b1
samer <samerbenmim@insat.u-carthage.tn>
2022-09-04 15:49:35 +0100

    537. Complex Number Multiplication

---
+++ Math/MEDIUM - 537. Complex Number Multiplication.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string complexNumberMultiply(string a, string b) {
+        string n1, n2, c1, c2;
+        int i = 0;
+        while (a[i] != '+')
+            n1 += a[i++];
+        c1 = a.substr(i + 1, a.size() - i - 2);
+        i = 0;
+        while (b[i] != '+')
+            n2 += b[i++];
+        c2 = b.substr(i + 1, b.size() - i - 2);
+        int n = stoi(n1), m = stoi(n2);
+        int c = stoi(c1), d = stoi(c2);
+        string ans = to_string(n * m - c * d) + "+" + to_string(m * c + n * d) + "i";
+        return ans;        
+    }
+};
+    
