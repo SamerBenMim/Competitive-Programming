a1688dca3025ea43993c1835b55fc706d1a139f7
samer <samerbenmim@insat.u-carthage.tn>
2022-10-10 08:44:32 +0100

    718. Maximum Length of Repeated Subarray

---
+++ Dynamic programming/MEDIUM - 718. Maximum Length of Repeated Subarray.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int dp[1004][1004];
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        int ans =0 ;
+        for(int i =1;i <= nums1.size() ; ++i){
+         for(int j =1;j <= nums2.size() ; ++j){
+            if(nums1[i-1]==nums2[j-1]) dp[i][j]=1+dp[i-1][j-1];
+            ans=max(ans,dp[i][j]);
+         }   
+        }
+        return ans;
+    }
+};
+
+/*  i
+   1 2 3 2 1
+ 3 0 0 3 0 0
+ 2 0 1 0 2 0
+ 1 1 0 0 0 1
+ 4 0 0 0 0 0
+ 7 0 0 0 0 0                 
+j
+        
