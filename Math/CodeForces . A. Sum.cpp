1de776ca7c1ca2caf2830b8e97acaf69f2dc6ea1
samer <samerbenmim@insat.u-carthage.tn>
2022-10-13 20:49:34 +0100

    A. Sum

---
+++ Math/CodeForces . A. Sum.cpp
@@ -0,0 +1,25 @@
+#include bitsstdc++.h
+ 
+using namespace std;
+#define fastInp cin.tie(0); cout.tie(0); ios_basesync_with_stdio(0);
+ 
+void solve() {
+int t[3];
+cint[0]t[1]t[2];
+sort(t,t+3);
+if(t[2]==t[1]+t[0])coutYESendl;
+else coutNOendl;
+}
+ 
+int main() {
+ 
+    fastInp
+    int t;
+    cint;
+    while(t--){
+    solve();
+    }
+    return 0;
+ 
+ 
+}
